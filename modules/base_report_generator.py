5b0894f280ea76b77cd4e76df6db216657bec4b3
Fluenoriph <fluenoriph@gmail.com>
2026-06-06 03:30:06 +1000

    Reapply "Start cli utility"
    This reverts commit 099db6d623a7c82bf4785d1f684b3b33175c49ce.

--- modules/base_report_generator.py
+++ modules/base_report_generator.py
@@ -6,14 +6,14 @@ from modules.real_time import CurrentTime
 
 
 class BaseReportGenerator(abc.ABC):
-    REPORT_FILE_TYPE = ('csv', 'json')
+    REPORT_FILE_TYPE = ('csv', 'json', 'html')
 
-    def __init__(self, result_data, report_path):
+    def __init__(self, result_data, report_path): # If default path !
         self.result_data = result_data
 
         self.create_report_file = lambda file_type: os.path.join(report_path,
                                                     f'report_{CurrentTime.get_time().replace(':', '-')}.{file_type}')
-
+                                                    # report type (name)
     @abc.abstractmethod
     def generate(self):
         pass
